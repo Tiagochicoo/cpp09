dcc784ca83f48938b1f4ba7dad522402b63bcc5e
tpereira <tpereira@student.42lisboa.com>
2023-11-25 19:10:58 +0000

    trying to get the algorithm to work

--- ex02/includes/PmergeMe.hpp
+++ ex02/includes/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 18:25:11 by tpereira          #+#    #+#             */
-/*   Updated: 2023/11/25 18:27:19 by tpereira         ###   ########.fr       */
+/*   Updated: 2023/11/25 19:10:37 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,80 +22,66 @@
 #include <list>
 #include <vector>
 
-class PmergeMe
-{
+class PmergeMe {
 private:
-	std::vector<int> _vector;
-	std::list<int> _list;
+  std::vector<int> _vector;
+  std::list<int> _list;
 
-	// void vectorFordJohnson(std::vector<int> _vector);
-	// void vectorFordJohnson(std::list<int> _list);
+  // void vectorFordJohnson(std::vector<int> _vector);
+  // void vectorFordJohnson(std::list<int> _list);
 
-	template <typename Container>
-	void fordJohnson(Container &sequence)
-	{
-		// Separate integers in half
-		typename Container::iterator it;
-		Container topHalf;
-		Container bottomHalf;
-		int size = sequence.size();
+  template <typename Container> void fordJohnson(Container &sequence) {
+    // Separate integers in half
+    typename Container::iterator it;
+    Container topHalf;
+    Container bottomHalf;
+    int size = sequence.size();
 
-		for (it = sequence.begin(); it != sequence.end(); ++it)
-		{
-			if (*it >= size)
-			{
-				topHalf.push_back(*it);
-			}
-			else
-			{
-				bottomHalf.push_back(-(*it));
-			}
-		}
+    for (it = sequence.begin(); it != sequence.end(); ++it) {
+      if (*it >= size) {
+        topHalf.push_back(*it);
+      } else {
+        bottomHalf.push_back(-(*it));
+      }
+    }
 
-		// Sort top half of integers in ascending order
-		if (std::is_same_v<Container, std::vector<int>>::value)
-		{
-			std::sort(topHalf.begin(), topHalf.end());
-		}
-		else if (std::is_same_v<Container, std::list<int>>::value)
-		{
-			topHalf.sort();
-		}
+    // Sort top half of integers in ascending order
+    if (std::is_same_v<Container, std::vector<int>>::value) {
+      std::sort(topHalf.begin(), topHalf.end());
+    } else if (std::is_same_v<Container, std::list<int>>::value) {
+      topHalf.sort();
+    }
 
-		// Sort bottom half of integers in descending order
-		if (std::is_same_v<Container, std::vector<int>>::value)
-		{
-			std::sort(bottomHalf.rbegin(), bottomHalf.rend());
-		}
-		else if (std::is_same_v<Container, std::list<int>>::value)
-		{
-			bottomHalf.sort(std::greater<>{});
-		}
+    // Sort bottom half of integers in descending order
+    if (std::is_same_v<Container, std::vector<int>>::value) {
+      std::sort(bottomHalf.rbegin(), bottomHalf.rend());
+    } else if (std::is_same_v<Container, std::list<int>>::value) {
+      bottomHalf.sort(std::greater<>{});
+    }
 
-		// Merge the sorted topHalf and bottomHalf integers
-		sequence.clear();
-		sequence.insert(sequence.end(), topHalf.begin(), topHalf.end());
+    // Merge the sorted topHalf and bottomHalf integers
+    sequence.clear();
+    sequence.insert(sequence.end(), topHalf.begin(), topHalf.end());
 
-		for (typename Container::iterator it = bottomHalf.begin();
-			 it != bottomHalf.end(); ++it)
-		{
-			sequence.push_back(-(*it));
-		}
-	}
+    for (typename Container::iterator it = bottomHalf.begin();
+         it != bottomHalf.end(); ++it) {
+      sequence.push_back(-(*it));
+    }
+  }
 
 public:
-	PmergeMe();
-	PmergeMe(const char *numbers);
-	PmergeMe(PmergeMe const &src);
-	~PmergeMe();
+  PmergeMe();
+  PmergeMe(const char *numbers);
+  PmergeMe(PmergeMe const &src);
+  ~PmergeMe();
 
-	PmergeMe &operator=(PmergeMe const &rhs);
+  PmergeMe &operator=(PmergeMe const &rhs);
 
-	void displayNumbers(std::string &when) const;
-	void measureTime();
+  void displayNumbers(std::string &when) const;
+  void measureTime();
 };
 
 std::ostream &operator<<(std::ostream &o, PmergeMe const &i);
 
-#endif /* ******************************************************** PMERGEME_H \
-		*/
+#endif /* ******************************************************** PMERGEME_H  \
+        */
--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -6,9 +6,10 @@
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 16:49:12 by tpereira          #+#    #+#             */
-/*   Updated: 2023/11/25 18:27:36 by tpereira         ###   ########.fr       */
+/*   Updated: 2023/11/25 19:10:17 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 // Here are a few container types you can consider testing with:
 
@@ -31,6 +32,205 @@ resizing.
  */
 #include "PmergeMe.hpp"
 
+
+// void insertionSortVector(std::vector<int>& sequence, size_t left, size_t right) {
+//     for (size_t i = left + 1; i <= right; ++i) {
+//         int key = sequence[i];
+//         size_t j = i;
+//         while (j > left && key < sequence[j - 1]) {
+//             sequence[j] = sequence[j - 1];
+//             --j;
+//         }
+//         sequence[j] = key;
+//     }
+// }
+
+// void mergeVector(std::vector<int>& sequence, size_t left, size_t middle, size_t right) {
+//     size_t n1 = middle - left + 1;
+//     size_t n2 = right - middle;
+
+//     std::vector<int> L(n1);
+//     std::vector<int> R(n2);
+
+//     for (size_t i = 0; i < n1; ++i)
+//         L[i] = sequence[left + i];
+//     for (size_t j = 0; j < n2; ++j)
+//         R[j] = sequence[middle + 1 + j];
+
+//     size_t i = 0;
+//     size_t j = 0;
+//     size_t k = left;
+
+//     while (i < n1 && j < n2) {
+//         if (L[i] <= R[j]) {
+//             sequence[k] = L[i];
+//             ++i;
+//         } else {
+//             sequence[k] = R[j];
+//             ++j;
+//         }
+//         ++k;
+//     }
+
+//     while (i < n1) {
+//         sequence[k] = L[i];
+//         ++i;
+//         ++k;
+//     }
+
+//     while (j < n2) {
+//         sequence[k] = R[j];
+//         ++j;
+//         ++k;
+//     }
+// }
+
+// void fordJohnsonVector(std::vector<int>& sequence, size_t left, size_t right) {
+//     const size_t threshold = 100;
+
+//     if (right - left + 1 <= threshold) {
+//         insertionSortVector(sequence, left, right);
+//     } else {
+//         size_t middle = left + (right - left) / 2;
+
+//         fordJohnsonVector(sequence, left, middle);
+//         fordJohnsonVector(sequence, middle + 1, right);
+
+//         mergeVector(sequence, left, middle, right);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+// void insertionSortDeque(std::deque<int>& sequence, size_t left, size_t right) 
+// {
+//     for (size_t i = left + 1; i <= right; ++i) {
+//         int key = sequence[i];
+//         size_t j = i;
+//         while (j > left && key < sequence[j - 1]) {
+//             sequence[j] = sequence[j - 1];
+//             --j;
+//         }
+//         sequence[j] = key;
+//     }
+// }
+
+// void mergeDeque(std::deque<int>& sequence, size_t left, size_t middle, size_t right) {
+//     size_t n1 = middle - left + 1;
+//     size_t n2 = right - middle;
+
+//     std::deque<int> L(sequence.begin() + left, sequence.begin() + left + n1);
+//     std::deque<int> R(sequence.begin() + middle + 1, sequence.begin() + middle + 1 + n2);
+
+//     size_t i = 0;
+//     size_t j = 0;
+//     size_t k = left;
+
+//     while (i < n1 && j < n2) {
+//         if (L[i] <= R[j]) {
+//             sequence[k] = L[i];
+//             ++i;
+//         } else {
+//             sequence[k] = R[j];
+//             ++j;
+//         }
+//         ++k;
+//     }
+
+//     while (i < n1) {
+//         sequence[k] = L[i];
+//         ++i;
+//         ++k;
+//     }
+
+//     while (j < n2) {
+//         sequence[k] = R[j];
+//         ++j;
+//         ++k;
+//     }
+// }
+
+// void fordJohnsonDeque(std::deque<int>& sequence, size_t left, size_t right) {
+//     const size_t threshold = 100;
+
+//     if (right - left + 1 <= threshold) {
+//         insertionSortDeque(sequence, left, right);
+//     } else {
+//         size_t middle = left + (right - left) / 2;
+
+//         fordJohnsonDeque(sequence, left, middle);
+//         fordJohnsonDeque(sequence, middle + 1, right);
+
+//         mergeDeque(sequence, left, middle, right);
+//     }
+// }
+
+
+// int main()
+// {
+// 	clock_t start, end;
+//     // Test with std::vector
+//     std::vector<int> vectorSequence;
+// 	vectorSequence.push_back(5);
+// 	vectorSequence.push_back(3);
+// 	vectorSequence.push_back(1);
+// 	vectorSequence.push_back(4);
+// 	vectorSequence.push_back(2);
+
+//     std::cout << "Original Vector: ";
+//     for (std::vector<int>::iterator it = vectorSequence.begin(); it != vectorSequence.end(); ++it)
+// 	{
+//     	std::cout << *it << " ";
+// 	}
+//     std::cout << std::endl;
+// 	start = clock();
+//     fordJohnsonVector(vectorSequence, 0, vectorSequence.size() - 1);
+// 	end = clock();
+//     std::cout << "Sorted Vector: ";
+//     for (std::vector<int>::iterator it = vectorSequence.begin(); it != vectorSequence.end(); ++it)
+// 	{
+//         std::cout <<  *it  << " ";
+//     }
+//     std::cout << std::endl << std::endl;
+
+//     // Test with std::deque
+//     std::deque<int> dequeSequence;
+// 	dequeSequence.push_back(5);
+// 	dequeSequence.push_back(3);
+// 	dequeSequence.push_back(1);
+// 	dequeSequence.push_back(4);
+// 	dequeSequence.push_back(2);
+
+//     std::cout << "Original Deque: ";
+//     for (std::deque<int>::iterator it = dequeSequence.begin(); it != dequeSequence.end(); ++it)
+// 	{
+//     	std::cout << *it << " ";
+// 	}
+
+//     std::cout << std::endl;
+
+//     fordJohnsonDeque(dequeSequence, 0, dequeSequence.size() - 1);
+
+//     std::cout << "Sorted Deque: ";
+//     for (std::deque<int>::iterator it = dequeSequence.begin(); it != dequeSequence.end(); ++it)
+// 	{
+//     	std::cout << *it << " ";
+// 	}
+//     std::cout << std::endl;
+
+//     return 0;
+// }
+
+
+
 int main(int argc, char **argv)
 {
 	if (argc < 2)
@@ -41,7 +241,7 @@ int main(int argc, char **argv)
 
 	PmergeMe pmerge(argv[1]);
 	std::string before = "Before: ";
-	// pmerge.displayNumbers(before);
+	pmerge.displayNumbers(before);
 	pmerge.measureTime();
 
 	return 0;
